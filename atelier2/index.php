53f62dd89233e2d90790300d0f76f7050ca714e4
toto_ <92277567+totoFR6974@users.noreply.github.com>
2025-11-05 11:02:59 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,7 +1,6 @@
 <?php
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
-
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
