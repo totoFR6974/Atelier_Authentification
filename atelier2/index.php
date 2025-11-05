d45d6f9022b9fb0ccc0af7544494bf6ff3a06045
toto_ <92277567+totoFR6974@users.noreply.github.com>
2025-11-05 11:04:14 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -4,7 +4,7 @@ session_start();
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']))
+if (isset($_COOKIE['authToken'])) {
     header('Location: page_admin.php');
     exit();
 }
